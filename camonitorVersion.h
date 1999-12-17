3fdbbccb50d00b06919ea5113d09ecd69ad651f0
Janet B. Anderson <jba@aps.anl.gov>
1999-12-17 09:16:29 -0600

    Fixed bugs in obtaining precision.

--- camonitorVersion.h
+++ camonitorVersion.h
@@ -4,7 +4,7 @@
 
 static char *camonitorVersionsccsId = "@(#) $Id$";
 
-#define camonitorVersion "@(#) camonitor Version 1.3.0  $Date$"
+#define camonitorVersion "@(#) camonitor Version 1.3.1  $Date$"
 
 #endif /* INCcamonitorVersionh */
 
